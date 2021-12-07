19b677b0f5a50c2abc76702bac13c6a2fa3b7925
emzed7269 <78227232+emzed7269@users.noreply.github.com>
2021-12-07 19:52:46 +0100

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,12 @@
+def day4data(filename: str) -> list:
+  with open(filename) as input :
+      bingos = input.read().strip()
+      bingos = re.split('\n',bingos)
+      bingos = list(map(str,bingos))
+      order = re.split(',',bingos[0])
+      order = [int(i) for i in order]
+      bingos = [bingos[n] for n in range(len(bingos)) if n>0 and bingos[n] != '']
+      bingos = [re.split(' ',bingos[i]) for i in range(len(bingos))]
+      bingos = [[int(bingos[n][i]) for i in range(len(bingos[n])) if bingos[n][i] != ''] for n in range(len(bingos))]
+      print(order)
+      return bingos
